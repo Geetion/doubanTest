1f1f6638d8a1407de993bac7c3666c3e63112d9d
Cc极生 <ccgeetion@gmail.com>
2016-04-02 15:06:20 +0800

    Initial Commit

---
+++ douban/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  douban
+//
+//  Created by Geetion on 16/4/2.
+//  Copyright © 2016年 Geetion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ douban/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  douban
+//
+//  Created by Geetion on 16/4/2.
+//  Copyright © 2016年 Geetion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
