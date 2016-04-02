49700aa97b7887b3f5e85d88375a1d7c9a065536
Cc极生 <ccgeetion@gmail.com>
2016-04-02 16:18:49 +0800

    first commit

--- douban/ViewController.m
+++ douban/ViewController.m
@@ -7,21 +7,47 @@
 //
 
 #import "ViewController.h"
-
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
+NSString *mURL = @"https://api.douban.com/v2/book/search";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)searchBtOnClick:(UIButton *)sender {
+    
+    NSString *keyword = [self.searchField text];
+    
+    if (keyword.length != 0) {
+        
+        [self getSearchResult:keyword];
+    }
+    
+}
+
+/*
+ 获取从服务器传回的数据
+ @param:keyword 传入关键字
+ */
+-(void)getSearchResult:(NSString*)keyword{
+    
+    NSString *getUrl = [mURL stringByAppendingString:@"?q="];
+    getUrl = [getUrl stringByAppendingString:keyword];
+    NSString *Url = [getUrl stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+
+    AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+    
+    [session GET:Url parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"%@",responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@",error);
+    }];
 }
 
 @end
